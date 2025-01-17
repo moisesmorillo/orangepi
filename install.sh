fa4bc6fa85765301b96a58ab020266bab72f9f96
Moisés Morillo <mmorillodev@pm.me>
2025-01-16 20:37:59 -0500

    feat: added instructions to delete k3s

--- install.sh
+++ install.sh
@@ -7,6 +7,8 @@ curl -sfL https://get.k3s.io | K3S_KUBECONFIG_MODE="644" sh -
 # Alternatively, you can use the following command to install k3s without flannel CNI in case you want to use another
 # CNI like calico
 # curl -sfL https://get.k3s.io | K3S_KUBECONFIG_MODE="644" sh -s - --flannel-backend none
+# To uninstall k3s, run the following command
+# sudo /usr/local/bin/k3s-uninstall.sh
 
 find "$KUBE_DIR" -type f -name "*.yaml" | while read -r yaml; do
   echo "Applying $yaml"
