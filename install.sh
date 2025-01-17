3933262e477f59afc6593d53c2b6a7eaae6e30ad
Moisés Morillo <mmorillodev@pm.me>
2025-01-16 20:36:49 -0500

    feat: improved install script to include kube dir yamls and better k3s install

--- install.sh
+++ install.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
 
-curl -sfL https://get.k3s.io | sh -
+KUBE_DIR="./kubernetes"
+
+# Install k3s
+curl -sfL https://get.k3s.io | K3S_KUBECONFIG_MODE="644" sh -
+# Alternatively, you can use the following command to install k3s without flannel CNI in case you want to use another
+# CNI like calico
+# curl -sfL https://get.k3s.io | K3S_KUBECONFIG_MODE="644" sh -s - --flannel-backend none
+
+find "$KUBE_DIR" -type f -name "*.yaml" | while read -r yaml; do
+  echo "Applying $yaml"
+  kubectl apply -f "$yaml"
+done
+
+echo "All yaml files applied"
