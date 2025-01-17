36e0143810f330cf97e4a2bfd133d9cc994bef32
Moisés Morillo <mmorillodev@pm.me>
2025-01-17 17:21:33 -0500

    fix: added kubeconfig var and disable traefix properly

--- install.sh
+++ install.sh
@@ -8,7 +8,9 @@ sudo apt install -y \
 KUBE_DIR="./kubernetes"
 
 # Install k3s
-curl -sfL https://get.k3s.io | K3S_KUBECONFIG_MODE="644" sh - --disable=traefik
+curl -sfL https://get.k3s.io | K3S_KUBECONFIG_MODE="644" sh -s - --disable=traefik
+
+grep -qxF "export KUBECONFIG=/etc/rancher/k3s/k3s.yaml" ~/.bashrc || echo "export KUBECONFIG=/etc/rancher/k3s/k3s.yaml" >>~/.bashrc
 
 # Install helm
 curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash
